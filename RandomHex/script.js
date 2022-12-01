5542247e707cbc29cc29ad30122953408c4b677c
elek8871 <elekmichelle@gmail.com>
2022-12-01 10:08:43 -0700

    random color generator

---
+++ RandomHex/script.js
@@ -0,0 +1,18 @@
+
+// numbers will from 0-9 and letters a-f [toString(16)]
+// use a substring to slice 6 char from string
+
+// color = Math.random().toString(16).substring(2, 8)
+
+const hexColor = document.querySelector(".color")
+const generate = document.querySelector(".button")
+
+const generateColor  = () =>{
+    const randomColor = Math.random().toString(16).substring(2, 8)
+    document.body.style.backgroundColor = "#" + randomColor
+    hexColor.innerHTML =  "#" + randomColor
+}
+
+generate.addEventListener("click", generateColor)
+
+generateColor()
--- RandomNumGenerator/script.js
+++ RandomNumGenerator/script.js
@@ -10,3 +10,4 @@ const generateNumber =()=>{
 }
 
 button.addEventListener("click", generateNumber)
+
